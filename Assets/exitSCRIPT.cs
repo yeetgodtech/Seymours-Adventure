0093d4fba8f532687b5f309fea66c9446d62243a
yeetgodmcneckass <yeetgodmcneckass@users.noreply.github.com>
2019-06-20 14:20:57 -0400

    Initial testBuild and asset commit
    Contains the first test build for Seymour's Adventure. Only shows off menus and a bit of the first level. Use Alt+F4 if you go into the options menu.

---
+++ Assets/exitSCRIPT.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.SceneManagement;
+using UnityEngine;
+
+public class exitSCRIPT : MonoBehaviour
+{
+    // Exits Game
+    public void exitGame(string sceneExit)
+    {
+        Application.Quit();
+        Debug.Log("Game has been successfully exited!");
+    }
+
+}
